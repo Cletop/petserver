14ec4677a8409f0b8f4e3454d96859c2ed427676
lishiwen <lishiwen@wclound.cn>
2022-08-17 21:58:12 +0800

    feat: define message types

---
+++ domains/im/message/message_enum.go
@@ -0,0 +1,17 @@
+package message
+
+const (
+	TEXT_MESSAGE      string = "text"
+	IMAGE_MESSAGE     string = "image"
+	RICH_TEXT_MESSAGE string = "rich_text"
+	VOICE_MESSAGE     string = "voice"
+	VIDEO_MESSAGE     string = "video"
+	LOCATION_MESSAGE  string = "location"
+	FILE_MESSAGE      string = "file"
+	READ_MESSAGE      string = "read"
+	READ_ACK_MESSAGE  string = "read_ack"
+	CUSTOM_MESSAGE    string = "custom"
+	UNKNOWN_MESSAGE   string = "unknown"
+	PING_MESSAGE      string = "ping"
+	PONG_MESSAGE      string = "pong"
+)
